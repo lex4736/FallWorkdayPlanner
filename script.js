6f592af8441c2b73c80b69aac40ad44e43e64117
Alexis Arando <lex4736@gmail.com>
2020-10-31 16:41:44 -0700

    Added function to determine and display current date and time

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ let sevenPm = $("#19pm");
 let hour = moment().hours();
 let userInput;
 let hourSpan;
+
+// Date and Hour
+
+let interval = setInterval(function() {
+    let momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
+  
+  function initPage() {
+  
+    console.log("Current Hour " + hour);
+    let init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nineAm.val(init9);
+  
+    let init10 = JSON.parse(localStorage.getItem("10:00 am"))
+    tenAm.val(init10);
+    
+    let init11 = JSON.parse(localStorage.getItem("11:00 am"))
+    elevenAm.val(init11);
+    
+    let init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+    twelvePm.val(init12);
+    
+    let init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+    onePm.val(init1);
+    
+    let init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+    twoPm.val(init2);
+    
+    let init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+    threePm.val(init3);
+   
+    let init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+    fourPm.val(init4);
+    
+    let init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+    fivePm.val(init5);
+    
+    let init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+    sixPm.val(init6);
+    
+    let init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+    sevenPm.val(init7);
+  } 
